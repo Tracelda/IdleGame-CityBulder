53bb1780762aa99968d4024c5d1c1f0a2cc525ba
Edward Goodhew <ejgoodhew@btinternet.com>
2018-11-08 19:03:19 +0000

    Static made

---
+++ Idle Game - City Builder/Assets/Scripts/InfoStatic.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoStatic
+{
+    // Score totals
+    public static int population;
+    public static int gold;
+
+    // Upgrades Bought & Amounts
+    public static bool housebought;
+    public static int houses;
+
+    public static bool marketbought;
+    public static int markets;
+
+    public static bool minebought;
+    public static int mines;
+
+
+    public static int Population
+    {
+        get
+        {
+            return population;
+        }
+        set
+        {
+            population = value;
+        }
+    }
+
+    public static int Gold
+    {
+        get
+        {
+            return gold;
+        }
+        set
+        {
+            gold = value;
+        }
+    }
+
+
+    // House
+    public static bool HousesBought
+    {
+        get
+        {
+            return housebought;
+        }
+        set
+        {
+            housebought = value;
+        }
+    }
+    public static int Houses
+    {
+        get
+        {
+            return houses;
+        }
+        set
+        {
+            houses = value;
+        }
+    }
+
+    // Market
+    public static bool Marketbought
+    {
+        get
+        {
+            return marketbought;
+        }
+        set
+        {
+            marketbought = value;
+        }
+    }
+    public static int Markets
+    {
+        get
+        {
+            return markets;
+        }
+        set
+        {
+            markets = value;
+        }
+    }
+
+
+}
---
+++ Idle Game - City Builder/Assets/Scripts/ScoreScrpt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScrpt : MonoBehaviour {
+
+    public InfoStatic InfoStatic;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Gold:" + InfoStatic.gold);
+        Debug.Log("Population:" + InfoStatic.population);
+    }
+
+    public void UpdateGoldOnBtn()
+    {
+        InfoStatic.gold++;
+    }
+
+    public void UpdatePopulationOnBtn()
+    {
+        InfoStatic.population++;
+    }
+}
